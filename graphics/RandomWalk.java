917be69b29bdd50dc4ccd61cbdff5eb10e40b08a
vannafer <99269165+vannafer@users.noreply.github.com>
2022-12-20 08:57:04 -0500

    erwqerwqer

---
+++ graphics/RandomWalk.java
@@ -0,0 +1,14 @@
+package graphics;
+
+public class RandomWalk{
+    private int stepX;
+    private int stepY;
+
+
+    public static void main(String[] args){
+        Ellipse egg = new Ellipse(150, 150, 10, 10);
+        egg.setColor(Color.RED);
+        egg.fill();  
+        
+    }
+}
---
+++ graphics/testingJavaGraphics.java
@@ -0,0 +1,22 @@
+package graphics;
+public class testingJavaGraphics{
+    public static void main(String[] args){
+        /*
+        Rectangle box = new Rectangle(5, 10, 60, 80);
+        box.draw();
+        Ellipse egg = new Ellipse(100, 100, 10, 10);
+        egg.setColor(Color.RED);
+        egg.fill();  
+        box.translate(40.0,10.0);
+        box.grow(10.0,5.0);
+        Line segment = new Line(30, 20, 50, 80);
+        segment.draw();
+        Text greeting = new Text(40, 40, "Hello");   
+        greeting.draw();
+        Picture pic = new Picture("frog.png");
+        pic.draw();
+        pic.translate(40, 60);
+        */
+
+    }
+}
