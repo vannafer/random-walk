9f3fa5e815514a9aa0f795405b68bcfd075cbf58
vannafer <99269165+vannafer@users.noreply.github.com>
2022-12-21 08:56:59 -0500

    Update RandomWalk.java
    5tuydynjuuiytiyti

--- graphics/RandomWalk.java
+++ graphics/RandomWalk.java
@@ -4,11 +4,28 @@ public class RandomWalk{
     private int stepX;
     private int stepY;
 
-
     public static void main(String[] args){
-        Ellipse egg = new Ellipse(150, 150, 10, 10);
+        //bg
+        Rectangle bg = new Rectangle(0,0,300,300);
+        bg.setColor(Color.BLUE);
+        bg.fill();  
+
+        //the thing escaping
+        Ellipse egg = new Ellipse(145, 145, 10, 10);
         egg.setColor(Color.RED);
         egg.fill();  
+
+        //the cell
+        Ellipse cell = new Ellipse(50, 50, 200, 200);
+        cell.draw();
+
+        while(egg.getX() ){
+            stepX = (int) Math.random()*20 -10;
+            stepY = (int) Math.random()*20 -10;
+            egg.Translate(stepX, stepY);
+        }
+
+
         
     }
 }
